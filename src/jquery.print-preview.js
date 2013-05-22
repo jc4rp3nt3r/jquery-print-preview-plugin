14c89475a65bdbf5e32f880e041b2b0990b783ec
James Carpenter <nomadic.design@gmail.com>
2013-05-21 20:41:00 -0400

    Extra Character cleanup

--- src/jquery.print-preview.js
+++ src/jquery.print-preview.js
@@ -7,9 +7,9 @@
  *
  * Date: Wed Jan 25 00:00:00 2012 -000
  */
- 
-(function($) { 
-    
+
+(function($) {
+
 	// Initialization
 	$.fn.printPreview = function(oSelector) {
 		this.each(function() {
@@ -22,7 +22,7 @@
 		});
 		return this;
 	};
-    
+
     // Private functions
     var mask, size, print_modal, print_controls;
     $.printPreview = {
@@ -85,7 +85,7 @@
             $('img', print_frame_ref).load(function() {
                 print_frame.height($('body', print_frame.contents())[0].scrollHeight);
             });
-            
+
             // Position modal
             starting_position = $(window).height() + $(window).scrollTop();
             var css = {
@@ -101,7 +101,7 @@
                     print_controls.fadeIn('slow').focus();
                 });
             print_frame.height($('body', print_frame.contents())[0].scrollHeight);
-            
+
             // Bind closure
             $('a', print_controls).bind('click', function(e) {
                 e.preventDefault();
@@ -109,7 +109,7 @@
                 else { $.printPreview.distroyPrintPreview(); }
             });
     	},
-    	
+
     	distroyPrintPreview: function() {
     	    print_controls.fadeOut(100);
     	    print_modal.animate({ top: $(window).scrollTop() - $(window).height(), opacity: 1}, 400, 'linear', function(){
@@ -118,57 +118,57 @@
     	    });
     	    mask.fadeOut('slow', function()  {
     			mask.remove();
-    		});				
+    		});
 
     		$(document).unbind("keydown.printPreview.mask");
     		mask.unbind("click.printPreview.mask");
     		$(window).unbind("resize.printPreview.mask");
 	    },
-	    
+
     	/* -- Mask Functions --*/
 	    loadMask: function() {
 	        size = $.printPreview.sizeUpMask();
             mask = $('<div id="print-modal-mask" />').appendTo($('body'));
-    	    mask.css({				
-    			position:           'absolute', 
-    			top:                0, 
+    	    mask.css({
+    			position:           'absolute',
+    			top:                0,
     			left:               0,
     			width:              size[0],
     			height:             size[1],
     			display:            'none',
-    			opacity:            0,					 		
+    			opacity:            0,
     			zIndex:             9999,
     			backgroundColor:    '#000'
     		});
-	
+
     		mask.css({display: 'block'}).fadeTo('400', 0.75);
-    		
+
             $(window).bind("resize..printPreview.mask", function() {
 				$.printPreview.updateMaskSize();
 			});
-			
+
 			mask.bind("click.printPreview.mask", function(e)  {
 				$.printPreview.distroyPrintPreview();
 			});
-			
+
 			$(document).bind("keydown.printPreview.mask", function(e) {
 			    if (e.keyCode == 27) {  $.printPreview.distroyPrintPreview(); }
 			});
         },
-    
+
         sizeUpMask: function() {
             if ($.browser.msie) {
             	// if there are no scrollbars then use window.height
             	var d = $(document).height(), w = $(window).height();
             	return [
             		window.innerWidth || 						// ie7+
-            		document.documentElement.clientWidth || 	// ie6  
+            		document.documentElement.clientWidth || 	// ie6
             		document.body.clientWidth, 					// ie6 quirks mode
             		d - w < 20 ? w : d
             	];
             } else { return [$(document).width(), $(document).height()]; }
         },
-    
+
         updateMaskSize: function() {
     		var size = $.printPreview.sizeUpMask();
     		mask.css({width: size[0], height: size[1]});
